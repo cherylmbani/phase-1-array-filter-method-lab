ffea3c1e39d6debdbd10dd8a3cb698a60fd5d302
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-14 14:23:48 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const driversMatch = drivers.filter(function findMatching(driversarray, name){
+
+    return driversMatch.toLowerCase()===name.toLowerCase();
+});
+
+function findMatching(driversArray, nameToMatch){
+    const result = driversArray.filter(function(driverName){
+        return driverName.toLowerCase() === nameToMatch;
+    });
+    return result;
+
+}
+const drivers=["bobby", "cheryl", "bobby"];
